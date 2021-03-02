6ef1b29dc553ad481e19184809623013f367b591
Lea Rosema <terabaud@gmail.com>
2021-03-02 23:52:09 +0100

    chore: remove boxes

--- src/glsl/fragment-shader.glsl
+++ src/glsl/fragment-shader.glsl
@@ -1,7 +1,9 @@
+#pragma glslify: noise = require('glsl-noise/simplex/2d')
+
 uniform float time;
 uniform vec2 resolution;
 varying vec2 vUv;
 
 void main() {
-  gl_FragColor = vec4(sin(vUv.x + time), 0., 1., 1.);
+  gl_FragColor = vec4(vec3(sin(time), 0, 1.) * noise(vUv.xy + vec2(time * .1, 0)), 1.);
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,17 +2,12 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import { Canvas } from 'react-three-fiber';
 
-import { Box } from './components/box';
 import { ShaderPlane } from './components/shader-plane';
 import fragmentShader from './glsl/fragment-shader.glsl';
 import vertexShader from './glsl/vertex-shader.glsl';
 
 ReactDOM.render(
   <Canvas>
-    <ambientLight />
-    <pointLight position={[10, 10, 10]} />
-    <Box position={[-1.2, 0, 2]} />
-    <Box position={[1.2, 0, 2]} />
     <ShaderPlane
       fragmentShader={fragmentShader}
       vertexShader={vertexShader}
